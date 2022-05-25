948f3012968f0836b030ddc499578d18cb949845
Leon Heid <leon.heid@quibiq.ch>
2022-05-25 13:36:07 +0200

    Created UI

--- Abschlussprojekt/Form1.Designer.cs
+++ Abschlussprojekt/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Abschlussprojekt
 {
-    partial class Form1
+    partial class Weather
     {
         /// <summary>
         /// Required designer variable.
@@ -28,24 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Weather));
             this.addressinput = new System.Windows.Forms.TextBox();
+            this.clearinput = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // addressinput
             // 
-            this.addressinput.Location = new System.Drawing.Point(12, 12);
+            this.addressinput.Location = new System.Drawing.Point(35, 12);
             this.addressinput.Name = "addressinput";
-            this.addressinput.Size = new System.Drawing.Size(497, 22);
+            this.addressinput.Size = new System.Drawing.Size(525, 22);
             this.addressinput.TabIndex = 0;
+            this.addressinput.Text = "Address of the Location";
+            this.addressinput.TextChanged += new System.EventHandler(this.addressinput_TextChanged);
             // 
-            // Form1
+            // clearinput
             // 
+            this.clearinput.Location = new System.Drawing.Point(705, 12);
+            this.clearinput.Name = "clearinput";
+            this.clearinput.Size = new System.Drawing.Size(75, 25);
+            this.clearinput.TabIndex = 1;
+            this.clearinput.Text = "Clear";
+            this.clearinput.UseVisualStyleBackColor = true;
+            this.clearinput.Click += new System.EventHandler(this.clearinput_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(125, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(235, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(125, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(435, 61);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(125, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(635, 61);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(125, 22);
+            this.textBox4.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(595, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Weather";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Weather
+            // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.clearinput);
             this.Controls.Add(this.addressinput);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Weather";
+            this.Text = "Weather";
+            this.Load += new System.EventHandler(this.Weather_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -54,6 +127,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox addressinput;
+        private System.Windows.Forms.Button clearinput;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Abschlussprojekt/Form1.cs
+++ Abschlussprojekt/Form1.cs
@@ -10,11 +10,39 @@ using System.Windows.Forms;
 
 namespace Abschlussprojekt
 {
-    public partial class Form1 : Form
+    public partial class Weather : Form
     {
-        public Form1()
+        public Weather()
         {
             InitializeComponent();
+        }
+
+        private void addressinput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearinput_Click(object sender, EventArgs e)
+        {
+            addressinput.Text = String.Empty;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String input = addressinput.Text;
+            String geolocation = "https://api.myptv.com/geocoding/v1/locations/by-text?searchText=" + input + "&apiKey=MGUxZGNjZGM1NTlkNDFhMjgxYjkzZGIwZTU5NjMyYmU6ZTFjZjE5OGMtMTRhYi00MTY5LThjMDItOGMxOTJmZTAzODRj";
+            textBox1.Text = input;
+            textBox2.Text = geolocation;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Weather_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
