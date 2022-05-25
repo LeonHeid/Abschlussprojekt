365b4907d4d983727cb90fc03dec1352e6a3f5c2
Leon Heid <leon.heid@quibiq.ch>
2022-05-25 15:44:20 +0200

    Created Geolocation API Request. Output Coordinates from Geolocation API

--- Abschlussprojekt/Form1.cs
+++ Abschlussprojekt/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +14,7 @@ namespace Abschlussprojekt
 {
     public partial class Weather : Form
     {
+        Dictionary<string, string> stationDict;
         public Weather()
         {
             InitializeComponent();
@@ -25,14 +28,37 @@ namespace Abschlussprojekt
         private void clearinput_Click(object sender, EventArgs e)
         {
             addressinput.Text = String.Empty;
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+            textBox3.Text = String.Empty;
+            textBox4.Text = String.Empty;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
             String input = addressinput.Text;
-            String geolocation = "https://api.myptv.com/geocoding/v1/locations/by-text?searchText=" + input + "&apiKey=MGUxZGNjZGM1NTlkNDFhMjgxYjkzZGIwZTU5NjMyYmU6ZTFjZjE5OGMtMTRhYi00MTY5LThjMDItOGMxOTJmZTAzODRj";
-            textBox1.Text = input;
-            textBox2.Text = geolocation;
+            if (input != String.Empty)
+            {
+                String geolocation = "https://api.myptv.com/geocoding/v1/locations/by-text?searchText=" + input + "&apiKey=MGUxZGNjZGM1NTlkNDFhMjgxYjkzZGIwZTU5NjMyYmU6ZTFjZjE5OGMtMTRhYi00MTY5LThjMDItOGMxOTJmZTAzODRj";
+                //textBox1.Text = input;
+                //textBox2.Text = geolocation;
+
+
+                HttpClient client = new HttpClient();
+                stationDict = new Dictionary<string, string>();
+
+                string res = await client.GetStringAsync(geolocation);
+                var jsonDataSingle = JObject.Parse(res);
+
+
+                textBox1.Text = (string)jsonDataSingle["locations"][0]["referencePosition"]["latitude"];
+                textBox2.Text = (string)jsonDataSingle["locations"][0]["referencePosition"]["longitude"];
+            }
+            else if (input == String.Empty)
+            {
+                addressinput.Text = "NO INPUT";
+            }
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
