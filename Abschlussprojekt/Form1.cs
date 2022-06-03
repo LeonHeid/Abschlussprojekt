229e09d6d728dd1bb2919655f770ecad801052fd
Leon Heid <leon.heid@quibiq.ch>
2022-06-03 10:58:05 +0200

    Added Json converter for Logic App Data

--- Abschlussprojekt/Form1.cs
+++ Abschlussprojekt/Form1.cs
@@ -1,7 +1,11 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Abschlussprojekt
@@ -54,12 +58,12 @@ namespace Abschlussprojekt
                     textBox1.Text = (string)geoJsonDataSingle["locations"][0]["referencePosition"]["latitude"];
                     textBox2.Text = (string)geoJsonDataSingle["locations"][0]["referencePosition"]["longitude"];
 
-                    String latitude = (string)geoJsonDataSingle["locations"][0]["referencePosition"]["latitude"];
-                    String longitude = (string)geoJsonDataSingle["locations"][0]["referencePosition"]["longitude"];
+                    String resLatitude = (string)geoJsonDataSingle["locations"][0]["referencePosition"]["latitude"];
+                    String resLongitude = (string)geoJsonDataSingle["locations"][0]["referencePosition"]["longitude"];
 
-                    String weather = "https://api.tomorrow.io/v4/timelines?location=" + latitude + "," + longitude + "&fields=temperature,precipitationIntensity,precipitationProbability,precipitationType&timesteps=1m&units=metric&apikey=aw5TpIHGOpwXGncccwayTkiVZ2IyYrZ2";
+                    String weather = "https://api.tomorrow.io/v4/timelines?location=" + resLatitude + "," + resLongitude + "&fields=temperature,precipitationIntensity,precipitationProbability,precipitationType&timesteps=1m&units=metric&apikey=aw5TpIHGOpwXGncccwayTkiVZ2IyYrZ2";
 
-                    //textBox4.Text = weather;
+                    //textBox5.Text = weather;
 
                     HttpClient weatherClient = new HttpClient();
 
@@ -74,47 +78,89 @@ namespace Abschlussprojekt
                     textBox4.Text = (string)weatherJsonDataSingle["data"]["timelines"][0]["intervals"][0]["startTime"];
 
 
-                    int pIntensityString = (int)weatherJsonDataSingle["data"]["timelines"][0]["intervals"][0]["values"]["precipitationIntensity"];
-                    int pProbabilityString = (int)weatherJsonDataSingle["data"]["timelines"][0]["intervals"][0]["values"]["precipitationProbability"];
+                    int pIntensityInt = (int)weatherJsonDataSingle["data"]["timelines"][0]["intervals"][0]["values"]["precipitationIntensity"];
+                    int pProbabilityInt = (int)weatherJsonDataSingle["data"]["timelines"][0]["intervals"][0]["values"]["precipitationProbability"];
 
                     //int pIntensityInt = Int32.Parse(pIntensityString);
                     //int pProbabilityInt = Int32.Parse(pProbabilityString);
 
-
+                    String resDescription = "";
                     String pSwitchStringType = (string)weatherJsonDataSingle["data"]["timelines"][0]["intervals"][0]["values"]["precipitationType"];
                     int pSwitchType = Int32.Parse(pSwitchStringType);
-                    if (pIntensityString == 0 && pProbabilityString == 0)
+                    if (pIntensityInt == 0 && pProbabilityInt == 0)
                     {
                         pSwitchType = 0;
                         pType.Text = "0";
                     }
-                    else if (pIntensityString != 0 && pProbabilityString != 0)
+                    else if (pIntensityInt != 0 && pProbabilityInt != 0)
                     {
 
                     }
                     switch (pSwitchType)
                     {
                         case 0:
-                            pDescription.Text = "Not Any";
+                            resDescription = "Not Any";
+                            pDescription.Text = resDescription;
                             break;
                         case 1:
-                            pDescription.Text = "Rain";
+                            resDescription = "Rain";
+                            pDescription.Text = resDescription;
                             break;
                         case 2:
-                            pDescription.Text = "Snow";
+                            resDescription = "Snow";
+                            pDescription.Text = resDescription;
                             break;
                         case 3:
-                            pDescription.Text = "Freezing Rain";
+                            resDescription = "Freezing Rain";
+                            pDescription.Text = resDescription;
                             break;
                         case 4:
-                            pDescription.Text = "Ice Pellets";
+                            resDescription = "Ice Pellets";
+                            pDescription.Text = resDescription;
                             break;
                         default:
                             break;
                     }
 
+                    /*HttpWebRequest webRequest;
+
+                    string requestParams = ""; //format information you need to pass into that string ('info={ "EmployeeID": [ "1234567", "7654321" ], "Salary": true, "BonusPercentage": 10}');
+
+                    webRequest = (HttpWebRequest)WebRequest.Create("http://example.com/xyz/php/api/createjob.php");
+
+                    webRequest.Method = "POST";
+                    webRequest.ContentType = "application/json";
+
+                    byte[] byteArray = Encoding.UTF8.GetBytes(requestParams);
+                    webRequest.ContentLength = byteArray.Length;
+                    using (Stream requestStream = webRequest.GetRequestStream())
+                    {
+                        requestStream.Write(byteArray, 0, byteArray.Length);
+                    }
+
+                    // Get the response.
+                    using (WebResponse response = webRequest.GetResponse())
+                    {
+                        using (Stream responseStream = response.GetResponseStream())
+                        {
+                            StreamReader rdr = new StreamReader(responseStream, Encoding.UTF8);
+                            string Json = rdr.ReadToEnd(); // response from server
+
+                        }
+                    }*/
+
+                    String resTemperature = (string)weatherJsonDataSingle["data"]["timelines"][0]["intervals"][0]["values"]["temperature"];
+                    String resTime = (string)weatherJsonDataSingle["data"]["timelines"][0]["intervals"][0]["startTime"];
+                    String resPIntensity = (string)weatherJsonDataSingle["data"]["timelines"][0]["intervals"][0]["values"]["precipitationIntensity"];
+                    String resPProbability = (string)weatherJsonDataSingle["data"]["timelines"][0]["intervals"][0]["values"]["precipitationProbability"];
 
 
+                    
+
+
+                    JsonAppData postJson = new JsonAppData { latitude = resLatitude, longitude = resLongitude, temperature = resTemperature, utcTime = resTime, pIntensity = resPIntensity, pProbability = resPProbability, pType = pSwitchStringType, description = resDescription };
+                    string postToAzure = JsonConvert.SerializeObject(postJson);
+                    //textBox5.Text = postToAzure;
                 }
                 else if (input == String.Empty)
                 {
@@ -141,6 +187,22 @@ namespace Abschlussprojekt
         private void pIntensity_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        class JsonAppData
+        {
+            public String latitude;
+            public String longitude;
+            public String temperature;
+            public String utcTime;
+            public String pIntensity;
+            public String pProbability;
+            public String pType;
+            public String description;
         }
     }
 }
